1f1bbc720d6edd35b10b07cba2a923423f219f00
Melvin Castillo <78450770+Melvin080@users.noreply.github.com>
2024-12-01 18:40:32 -0500

    cambios

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -6,28 +6,28 @@ document.addEventListener('DOMContentLoaded', () => {
             nombre: 'Sombrero vueltiao con la bandera de Colombia, 23 vueltas',
             precio: 250000,
             imagen: 'assets/img/feature_prod_01.jpg',
-            categoria: 'sombreros'
+            categoria: 'peces'
         },
         {
             id: 2,
             nombre: 'Sombrero vueltiao-machiembriao',
             precio: 150000,
             imagen: 'assets/img/sombrero4.jpg',
-            categoria: 'sombreros'
+            categoria: 'plantas'
         },
         {
             id: 3,
             nombre: 'Sombrero vueltiao colombiano 15 vueltas tricolor',
             precio: 250000,
             imagen: 'assets/img/feature_prod_02.jpg',
-            categoria: 'sombreros'
+            categoria: 'alimento'
         },
         {
             id: 4,
             nombre: 'Mochila wayuu azul 6',
             precio: 120000,
             imagen: 'assets/img/shop_04.jpg',
-            categoria: 'mochilas'
+            categoria: 'acuarios'
         },
         {
             id: 5,
@@ -41,7 +41,21 @@ document.addEventListener('DOMContentLoaded', () => {
             nombre: 'Hamaca',
             precio: 120000,
             imagen: 'assets/img/category_img_02.jpg',
-            categoria: 'hamacas'
+            categoria: 'sustratos'
+        },
+        {
+            id: 6,
+            nombre: 'Hamaca',
+            precio: 120000,
+            imagen: 'assets/img/category_img_02.jpg',
+            categoria: 'herramientas'
+        },
+        {
+            id: 6,
+            nombre: 'Hamaca',
+            precio: 120000,
+            imagen: 'assets/img/category_img_02.jpg',
+            categoria: 'decoracion'
         }
     ];
 
@@ -58,37 +72,29 @@ document.addEventListener('DOMContentLoaded', () => {
 
     function renderizarProductos() {
         DOMitems.innerHTML = "";
-
         const filtro = filtroSelect.value;
         const productosFiltrados = baseDeDatos.filter(producto => 
             filtro === "todas" || producto.categoria === filtro
         );
-
         productosFiltrados.forEach((info) => {
             const miNodo = document.createElement('div');
             miNodo.classList.add('card', 'col-sm-4');
-            
             const miNodoCardBody = document.createElement('div');
             miNodoCardBody.classList.add('card-body');
-            
             const miNodoTitle = document.createElement('h6');
             miNodoTitle.classList.add('card-title');
             miNodoTitle.textContent = info.nombre;
-            
             const miNodoImagen = document.createElement('img');
             miNodoImagen.classList.add('img-fluid');
             miNodoImagen.setAttribute('src', info.imagen);
-            
             const miNodoPrecio = document.createElement('p');
             miNodoPrecio.classList.add('card-text');
             miNodoPrecio.textContent = `${info.precio}${divisa}`;
-            
             const miNodoBoton = document.createElement('button');
             miNodoBoton.classList.add('btn', 'btn-primary');
             miNodoBoton.textContent = 'Agregar';
             miNodoBoton.setAttribute('marcador', info.id);
             miNodoBoton.addEventListener('click', anadirProductoAlCarrito);
-
             miNodoCardBody.appendChild(miNodoImagen);
             miNodoCardBody.appendChild(miNodoTitle);
             miNodoCardBody.appendChild(miNodoPrecio);
@@ -97,66 +103,53 @@ document.addEventListener('DOMContentLoaded', () => {
             DOMitems.appendChild(miNodo);
         });
     }
-
     // Obtén el contador del almacenamiento local
     let visitas = localStorage.getItem('contadorVisitas');
-
     // Si no hay visitas almacenadas, inicializa a 0
     if (!visitas) {
         visitas = 0;
     }
-
     // Incrementa el contador
     visitas++;
-
     // Guarda el nuevo contador en el almacenamiento local
     localStorage.setItem('contadorVisitas', visitas);
-
     // Muestra el contador en la página
     document.getElementById('contador').textContent = visitas;
-
     function anadirProductoAlCarrito(evento) {
         carrito.push(evento.target.getAttribute('marcador'));
         renderizarCarrito();
         guardarCarritoEnLocalStorage();
         handleCarritoValue(carrito.length);
     }
-
     function handleCarritoValue(value) {
         const carritoContainer = document.getElementById("carrito-value");
         carritoContainer.textContent = `${value}`;
     }
-
     function renderizarCarrito() {
         DOMcarrito.textContent = '';
         const carritoSinDuplicados = [...new Set(carrito)];
-
         carritoSinDuplicados.forEach((item) => {
             const miItem = baseDeDatos.filter((itemBaseDatos) => {
                 return itemBaseDatos.id === parseInt(item);
             });
-
             const numeroUnidadesItem = carrito.reduce((total, itemId) => {
                 return itemId === item ? total += 1 : total;
             }, 0);
-
             const miNodo = document.createElement('li');
             miNodo.classList.add('list-group-item', 'text-right', 'mx-2');
             miNodo.textContent = `${numeroUnidadesItem} x ${miItem[0].nombre} - ${miItem[0].precio}${divisa}`;
-            
             const miBoton = document.createElement('button');
             miBoton.classList.add('btn', 'btn-danger', 'mx-5');
             miBoton.textContent = 'X';
             miBoton.style.marginLeft = '1rem';
             miBoton.dataset.item = item;
             miBoton.addEventListener('click', borrarItemCarrito);
-            
             miNodo.appendChild(miBoton);
             DOMcarrito.appendChild(miNodo);
         });
         DOMtotal.textContent = calcularTotal();
     }
-
+    //borra carrito
     function borrarItemCarrito(evento) {
         const id = evento.target.dataset.item;
         carrito = carrito.filter((carritoId) => {
@@ -166,7 +159,7 @@ document.addEventListener('DOMContentLoaded', () => {
         guardarCarritoEnLocalStorage();
         handleCarritoValue(carrito.length);
     }
-
+    //calcular el total
     function calcularTotal() {
         return carrito.reduce((total, item) => {
             const miItem = baseDeDatos.filter((itemBaseDatos) => {
@@ -175,28 +168,26 @@ document.addEventListener('DOMContentLoaded', () => {
             return total + miItem[0].precio;
         }, 0).toFixed(2);
     }
-
+    //vaciar todos los elementos del carrito
     function vaciarCarrito() {
         carrito = [];
         renderizarCarrito();
         localStorage.clear();
     }
-
+    //guardar en local el carrito
     function guardarCarritoEnLocalStorage() {
         miLocalStorage.setItem('carrito', JSON.stringify(carrito));
     }
-
+    //cargar del local el carriro
     function cargarCarritoDeLocalStorage() {
         if (miLocalStorage.getItem('carrito') !== null) {
             carrito = JSON.parse(miLocalStorage.getItem('carrito'));
             handleCarritoValue(carrito.length);
         }
     }
-
     // Eventos
     DOMbotonVaciar.addEventListener('click', vaciarCarrito);
     filtroSelect.addEventListener('change', renderizarProductos);
-
     // Inicio
     cargarCarritoDeLocalStorage();
     renderizarProductos();
